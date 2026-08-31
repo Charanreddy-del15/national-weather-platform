a0b066f9da512ca9d5cb0df8fcdd85bff83dd885
Charanreddy-del15 <yogithareddyginni@gmail.com>
2026-09-01 00:39:46 +0530

    Change TileLayer source to Esri World Imagery

--- frontend/src/components/MapView.tsx
+++ frontend/src/components/MapView.tsx
@@ -64,9 +64,9 @@ export const MapView: React.FC<MapViewProps> = ({ mapEvents, onSelectEvent, sele
         scrollWheelZoom={true}
         className="w-full h-full z-0 bg-gray-950"
       >
-       <TileLayer
-  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-  url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png"
+      <TileLayer
+  attribution='&copy; <a href="https://www.esri.com/">Esri</a>'
+  url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
 />
         <MapRecenter events={mapEvents} />
 
