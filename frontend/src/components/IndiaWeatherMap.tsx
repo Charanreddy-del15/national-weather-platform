6127d273932d864be1da43e85c42c9519b57ce99
Charanreddy-del15 <yogithareddyginni@gmail.com>
2026-09-01 00:36:51 +0530

    Update map tile layer attribution and URL

--- frontend/src/components/IndiaWeatherMap.tsx
+++ frontend/src/components/IndiaWeatherMap.tsx
@@ -38,9 +38,9 @@ export const IndiaWeatherMap: React.FC<IndiaWeatherMapProps> = ({ events, onSele
         className="w-full h-full z-0 bg-slate-950"
       >
         <TileLayer
-          attribution='&copy; <a href="https://carto.com/">CARTO</a>'
-          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-        />
+  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+  url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png"
+/>
 
         {events.map((evt) => {
           const color = getMarkerColor(evt.severity, evt.event_category);
